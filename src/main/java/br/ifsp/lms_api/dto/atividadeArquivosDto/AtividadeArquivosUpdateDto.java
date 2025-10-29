3a4f114fc8d472c66028c30e42876734dce0dca4
Gabriel <gabrielxsouzacosta@gmail.com>
2025-10-28 22:20:03 -0300

    agora roda

--- src/main/java/br/ifsp/lms_api/dto/atividadeArquivosDto/AtividadeArquivosUpdateDto.java
+++ src/main/java/br/ifsp/lms_api/dto/atividadeArquivosDto/AtividadeArquivosUpdateDto.java
@@ -3,7 +3,6 @@ package br.ifsp.lms_api.dto.atividadeArquivosDto;
 import br.ifsp.lms_api.dto.atividadesDto.AtividadesUpdateDto;
 
 import java.util.Optional;
-import java.time.LocalDate;
 import java.util.List;
 
 
@@ -13,15 +12,6 @@ import java.util.List;
 public class AtividadeArquivosUpdateDto extends AtividadesUpdateDto {
     private Optional<List<String>> arquivosPermitidos = Optional.empty();
 
-    public AtividadeArquivosUpdateDto() {
-        super();
-    }
-
-    public AtividadeArquivosUpdateDto(Optional<String> tituloAtividade, Optional<String> descricaoAtividade, Optional<LocalDate> dataInicioAtividade,
-                                     Optional<LocalDate> dataFechamentoAtividade, Optional<Boolean> statusAtividade, Optional<List<String>> arquivosPermitidos) {
-        super(tituloAtividade, descricaoAtividade, dataInicioAtividade, dataFechamentoAtividade, statusAtividade);
-        this.arquivosPermitidos = arquivosPermitidos;
-    }
 
     public Optional<List<String>> getArquivosPermitidos() {
         return arquivosPermitidos;
--- src/main/java/br/ifsp/lms_api/dto/atividadeTextoDto/AtividadeTextoResponseDto.java
+++ src/main/java/br/ifsp/lms_api/dto/atividadeTextoDto/AtividadeTextoResponseDto.java
@@ -2,20 +2,10 @@ package br.ifsp.lms_api.dto.atividadeTextoDto;
 
 import br.ifsp.lms_api.dto.atividadesDto.AtividadesResponseDto;
 
-import java.time.LocalDate;
-
 public class AtividadeTextoResponseDto extends AtividadesResponseDto {
     private long numeroMaximoPalavras;
 
-    public AtividadeTextoResponseDto() {
-        super();
-    }
 
-    public AtividadeTextoResponseDto(Long idAtividade, String tituloAtividade, String descricaoAtividade, LocalDate dataInicioAtividade,
-            LocalDate dataFechamentoAtividade, Boolean statusAtividade, long numeroMaximoPalavras) {
-        super(idAtividade, tituloAtividade, descricaoAtividade, dataInicioAtividade, dataFechamentoAtividade, statusAtividade);
-        this.numeroMaximoPalavras = numeroMaximoPalavras;
-    }
 
     public long getNumeroMaximoPalavras() {
         return numeroMaximoPalavras;
