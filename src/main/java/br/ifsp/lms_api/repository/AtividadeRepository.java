95243f795340b531e4f9bc5703bb008165dc7518
dudaselvatti <dudaselvatti@hotmail.com>
2025-10-31 21:47:38 -0300

    Add @Repository annotation to repository interfaces for consistency

--- src/main/java/br/ifsp/lms_api/repository/AtividadeRepository.java
+++ src/main/java/br/ifsp/lms_api/repository/AtividadeRepository.java
@@ -3,7 +3,6 @@ package br.ifsp.lms_api.repository;
 import br.ifsp.lms_api.model.Atividade;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
-import java.util.List;
 
 @Repository
 public interface AtividadeRepository extends JpaRepository<Atividade, Long> {
--- src/main/java/br/ifsp/lms_api/repository/QuestoesRepository.java
+++ src/main/java/br/ifsp/lms_api/repository/QuestoesRepository.java
@@ -1,9 +1,10 @@
 package br.ifsp.lms_api.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
-
+import org.springframework.stereotype.Repository;
 import br.ifsp.lms_api.model.Questoes;
 
+@Repository
 public interface QuestoesRepository extends JpaRepository<Questoes, Long> {
     
 }
